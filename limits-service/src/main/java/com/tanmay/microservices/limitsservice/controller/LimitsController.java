04398d04b1ea1d58f316a5c9cfa80b1188dded68
Tanmay <tanmay7.49@gmail.com>
2021-05-29 21:13:43 +0530

    limit service checkin#1

---
+++ limits-service/src/main/java/com/tanmay/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.tanmay.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmay.microservices.limitsservice.bean.Limits;
+import com.tanmay.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+	@Autowired
+	private Configuration configuration;
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		//return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+	}
+}
