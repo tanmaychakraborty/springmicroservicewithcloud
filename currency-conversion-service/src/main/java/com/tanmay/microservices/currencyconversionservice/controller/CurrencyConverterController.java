2cf63930a26aa7bc0d99b72be852bc7f504e4282
Tanmay <tanmay7.49@gmail.com>
2021-06-01 17:21:35 +0530

    Checkin

---
+++ currency-conversion-service/src/main/java/com/tanmay/microservices/currencyconversionservice/controller/CurrencyConverterController.java
@@ -0,0 +1,33 @@
+package com.tanmay.microservices.currencyconversionservice.controller;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.tanmay.microservices.currencyconversionservice.bean.CurrencyConversion;
+
+@RestController
+public class CurrencyConverterController {
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion retriveCurrencyConversion(
+			@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
+		HashMap<String,String> uriVariables=new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversion> responseEntity=new RestTemplate().getForEntity(
+				"http://127.0.0.1:8000/currency-exchange/from/{from}/to/{to}"
+				, CurrencyConversion.class,uriVariables);
+		CurrencyConversion currencyConversion=responseEntity.getBody();
+		
+		return new CurrencyConversion(currencyConversion.getId(),from,to,quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnviroment());
+	}
+}
